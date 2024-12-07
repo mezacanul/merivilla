a497fa09aef3048c895a84e285273ad57cfc26c5
Jose Eduardo Meza Canul <Lalo@192.168.1.118>
2024-12-07 15:22:44 -0600

    Updated Hero color and fonts

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -7,15 +7,16 @@ export default function Hero(){
         <Box {...styles.blackOverlay}/>
         
         <Box zIndex={1}>
-          <Heading fontWeight={"200"} size={"3xl"} width={"45vw"} mb={"1rem"}>REVOLUCIONA TUS VENTAS INMOBILIARIAS EN 2024</Heading>
+          {/* <Heading fontWeight={"100"} size={"3xl"} width={"45vw"} mb={"1rem"}>REVOLUCIONA TUS VENTAS INMOBILIARIAS EN <Heading display={"inline"} fontWeight={500} size={"3xl"}>2025</Heading></Heading> */}
+          <Heading fontWeight={"100"} size={"3xl"} width={"45vw"} mb={"1rem"}>MAXIMIZA TU NEGOCIO INMOBILIARIO EN <Heading display={"inline"} fontWeight={500} size={"3xl"}>2025</Heading></Heading>
           <Text w={"30vw"} mb={"1rem"} fontWeight={"400"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt risus fermentum dui mollis, vitae suscipit mauris hendrerit.</Text>
           <GoldButton>INSCRIBETE HOY</GoldButton>
         </Box>
   
         <HStack {...styles.testimonial} boxShadow={"0px 10px 8px rgba(0,0,0,0.3)"}>
           <Box {...styles.testimonialText}>
-            <Text>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt risus fermentum dui mollis, vitae suscipit mauris".</Text>
-            <Text fontWeight={600}>Name, CEO</Text>
+            <Text fontSize={"0.9rem"} fontWeight={800} w={"75%"} margin={"auto"}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt risus fermentum dui mollis, vitae suscipit mauris".</Text>
+            <Text fontWeight={300} mt={"1rem"}>CEO de DOGE</Text>
           </Box>
   
           <Image {...styles.img} src={"face.jpg"}/>
@@ -42,11 +43,13 @@ export default function Hero(){
       width: "100%",
       height: "100%",
       top: "0", left: "0",
-      background: "rgba(0, 66, 104, 0.5)"
+      // background: "rgba(0, 66, 104, 0.5)"
+      background: "#21213b",
+      opacity: 0.7
     },
     testimonial: {
       width: "40rem",
-      background: "#ffffffb7",
+      background: "#ffffffd5",
       zIndex: 1,
       color: "black",
       py: "1.5rem",
